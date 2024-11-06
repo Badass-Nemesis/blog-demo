03969a94a7c837e094ec3069ea5189a186109a28
Badass-Nemesis <harshitanant29@gmail.com>
2024-11-06 23:56:28 +0530

    fixed jwt undefined read issue. wrote a check to see if jwt in the cookie is present or not

--- Backend/middleware/adminMiddleware.js
+++ Backend/middleware/adminMiddleware.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 const isAdmin = (req, res, next) => {
-  const token = req.cookies.jwt;
-
-  if (!token) {
+  if (!req.cookies || !req.cookies.jwt) {
     return res.status(403).send({ message: 'Access denied: No token provided' });
   }
+
+  const token = req.cookies.jwt;
 
   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
     if (err) {
       return res.status(403).send({ message: 'Access denied: Invalid token' });
     }
 
-    if (decoded.username !== 'admin') {
+    if (!decoded.admin) {
       return res.status(403).send({ message: 'Access denied: Admins only' });
     }
 
